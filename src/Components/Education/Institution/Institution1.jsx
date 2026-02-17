b6dd1aa032b05da1a38a341a71fb2cd257f2a294
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:19:18 +0530

    update

--- src/Components/Education/Institution/Institution1.jsx
+++ src/Components/Education/Institution/Institution1.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Institution1() {
   return (
+    // Left side of the Institution section, showing institution icon left side of the page...
+    // Showing additional data of the institution on right side of the page...
     <div>Institution1</div>
   )
 }
--- src/Components/Education/Institution/Institution2.jsx
+++ src/Components/Education/Institution/Institution2.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Institution2() {
   return (
+    // Right side of the Institution section, showing institution icon right side of the page...
+    // Showing additional data of the institution on left side of the page...
     <div>Institution2</div>
   )
 }
