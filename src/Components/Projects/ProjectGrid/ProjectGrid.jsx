428578aeb689c27f9b5343c168adbb7d510a4af1
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:24:45 +0530

    update

---
+++ src/Components/Projects/ProjectGrid/ProjectGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectGrid() {
+  return (
+    // ProjectGrid component, showing individual project details in a grid layout...
+    // This component will receive props such as title, image source, project link, live link, tools used, and description to display each project in a structured format within the Projects section.
+    <div>ProjectGrid</div>
+  )
+}
+
+export default ProjectGrid
