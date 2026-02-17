b0593fe2b595af24dffe0ce5d6ea819b49c3f310
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:22:22 +0530

    update

---
+++ src/Components/Home/Parts/HomeEdu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HomeEdu() {
+  return (
+    // HomeEdu section, showing various educational content related to the website's theme...
+    <div>HomeEdu</div>
+  )
+}
+
+export default HomeEdu
